43fcb7989b8e49f36532413801a39248f67e15a5
astefanini <alexandre.andre.stefanini@gmail.com>
2021-11-29 22:21:08 -0300

    Versão 1.0

--- cataquasetudo/app_principal/urls.py
+++ cataquasetudo/app_principal/urls.py
@@ -14,19 +14,18 @@ from app_principal.views import TransporteCreateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="app_principal/index.html")),
     path('cliente_list_todos', ClienteListTodosView.as_view(), name = 'cliente_list_todos'),
-    path('cliente_list/<str:id_usuario>/<str:id_senha>', views.ClienteListView, name = 'cliente_list'),
+    path('cliente_list/<int:id_codigo>', views.ClienteListView, name = 'cliente_list'),
     path('cliente_create', ClienteCreateView.as_view(), name = 'cliente_create'),    
-    # path('cliente_login/<str:id_usuario>/<str:id_senha>',  views.cliente_login, name="cliente_login"),
-    path('cliente_login',  views.cliente_login, name="cliente_login"),    
-
-    path('contrato_create', ContratoCreateView.as_view(), name = 'contrato_create'),        
-
+    path('contrato_create/<int:id_cliente>/<int:id_transporte>', views.ContratoCreateView, name = 'contrato_create'),            
+    path('contrato_cliente_list/<int:id_cliente>', views.ContratoClienteListView, name = 'contrato_cliente_list'),            
+    path('contrato_transporte_list/<int:id_veiculo>', views.ContratoTransporteListView, name = 'contrato_transporte_list'),                
     path('prestador_list_todos', PrestadorListTodosView.as_view(), name = 'prestador_list_todos'),    
-    path('prestador_list/<str:id_usuario>/<str:id_senha>', views.ClienteListView, name = 'cliente_list'),    
+    path('prestador_list/<int:id_codigo>', views.ClienteListView, name = 'cliente_list'),    
     path('prestador_create', PrestadorCreateView.as_view(), name = 'prestador_create'),
     path('prestador_login', TemplateView.as_view(template_name="app_principal/prestador_login.html")),
-    path('prestador_list/<str:id_usuario>/<str:id_senha>', views.PrestadorListView, name = 'prestador_list'),    
-    
-    path('transporte_create', TransporteCreateView.as_view(), name = 'transporte_create'),    
+    path('transporte_create/<int:id_prestador>', views.TransporteCreateView, name = 'transporte_create'),
+    path('transporte_list/<str:id_prestador>', views.TransporteListView, name = 'transporte_list'),
+    path('transporte_list_todos/<str:id_cliente>', views.TransporteListTodosView, name = 'transporte_list_todos'),    
+    path('transporte_contrato_list/<str:id_veiculo>', views.TransporteContratoListView, name = 'transporte_contrato_list'),    
 ]
 
--- cataquasetudo/app_principal/views.py
+++ cataquasetudo/app_principal/views.py
@@ -1,31 +1,34 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, HttpResponseRedirectBase
 from django.shortcuts import redirect, render
 from django.template import RequestContext
 from django.http import HttpResponse
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic.list import ListView
 from django.db.models import Q
 from django.views.generic import CreateView
 from app_principal.models import Cliente, Contrato, Prestador, Transporte
+from app_principal.forms import TransporteCreateForm
+from app_principal.forms import ContratoCreateForm
 
 
 # Create your views here.
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
 class ClienteListTodosView(ListView):
-    template_name = "app_principal/cliente_list.html"
+    template_name = "app_principal/cliente_list_todos.html"
     model = Cliente
     context_object_name = "cliente_list"
 
-def ClienteListView(request, id_usuario, id_senha):
+def ClienteListView(request, id_codigo):
     # Cria um objeto vazio chamado clientes
     cliente_list = None
 
     #Aplica o filtro sobre os objetos em Cliente
-    v_cliente_filtrado = Cliente.objects.filter(usuario=id_usuario, senha=id_senha)
+    v_cliente_filtrado = Cliente.objects.filter(codigo=id_codigo)
 
     # Verifica se foi encontrado algum registro
     if len(v_cliente_filtrado) > 0 :
@@ -34,7 +37,7 @@ def ClienteListView(request, id_usuario, id_senha):
         cliente_list = None
 
     # Retorna o template clientes_lista.html e passa o filtro como parâmetro
-    return render(request, 'app_principal/cliente_list.html', {'cliente_list': cliente_list})
+    return render(request, 'app_principal/cliente_list.html', {'cliente_list': cliente_list, })
 
 class ClienteCreateView(CreateView):
     template_name = "app_principal/cliente_create.html"
@@ -63,32 +66,53 @@ def cliente_login(request):
     context={'id_usuario':id_usuario, 'id_senha':id_senha}
     return render(request, 'app_principal/cliente_login.html', context=context)
 
-class ContratoCreateView(CreateView):
-    template_name = "app_principal/contrato_create.html"
-    model = Contrato
-    fields = [
-            'cliente',
-            'transporte',
-            'quantidade',
-            'moveis',
-            'eletro',
-            'metais',
-            'alvenaria',
-            'madeiramento',
-            'troncos',
-            'arbusto',
-            'grama'
-             ]
-    exclude = [
-            'codigo'
-            ]
+def ContratoCreateView(request, id_cliente, id_transporte):
+    form = ContratoCreateForm(request.POST)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('cliente_list_todos'))
+    else:
+       return render(request, "app_principal/contrato_create.html", {'form': form, 'id_cliente': id_cliente, 'id_transporte': id_transporte })
 
-    success_url = reverse_lazy("contrato_create")
+def ContratoClienteListView(request, id_cliente):
+    # Cria um objeto vazio chamado transporte_list
+    contrato_cliente_list = None
+
+    #Aplica o filtro sobre os objetos em Transporte
+    v_contrato_cliente_filtrado = Contrato.objects.filter(cliente=id_cliente)
+
+    # Verifica se foi encontrado algum registro
+    if len(v_contrato_cliente_filtrado) > 0 :
+        contrato_cliente_list = v_contrato_cliente_filtrado
+    else:
+        contrato_cliente_list = None
+
+    # Retorna o template transporte_lista.html e passa o filtro como parâmetro
+    return render(request, 'app_principal/contrato_cliente_list.html', {'contrato_cliente_list': contrato_cliente_list})
+
+def ContratoTransporteListView(request, id_veiculo):
+    # Cria um objeto vazio chamado transporte_list
+    transporte_list = None
+
+    #Aplica o filtro sobre os objetos em Transporte
+    v_transporte_filtrado = Transporte.objects.filter(codigo=id_veiculo)
+
+    # Verifica se foi encontrado algum registro
+    if len(v_transporte_filtrado) > 0 :
+        transporte_list = v_transporte_filtrado
+    else:
+        transporte_list = None
+
+    # Retorna o template transporte_lista.html e passa o filtro como parâmetro
+    return render(request, 'app_principal/contrato_transporte_list.html', {'transporte_list': transporte_list})
+
 
 class PrestadorListTodosView(ListView):
-    template_name = "app_principal/prestador_list.html"
+    template_name = "app_principal/prestador_list_todos.html"
     model = Prestador
-    context_object_name = "prestador_list"
+    context_object_name = "prestador_list_todos"
 
 def PrestadorListView(request, id_usuario, id_senha):
     # Cria um objeto vazio chamado clientes
@@ -129,26 +153,61 @@ class PrestadorCreateView(CreateView):
 
     success_url = reverse_lazy("prestador_list_todos")
 
-class TransporteCreateView(CreateView):
-    template_name = "app_principal/transporte_create.html"
-    model = Transporte
-    fields = [
-            'prestador',
-            'veiculo',
-            'capacidade',
-            'moveis',
-            'eletro',
-            'metais',
-            'alvenaria',
-            'madeiramento',
-            'troncos',
-            'arbusto',
-            'grama',
-            'preco'
-             ]
-    exclude = [
-            'codigo'
-            ]
 
-    success_url = reverse_lazy("contrato_create")
+def TransporteCreateView(request, id_prestador):
+    form = TransporteCreateForm(request.POST)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('prestador_list_todos'))
+    else:
+       return render(request, "app_principal/transporte_create.html", {'form': form, 'id_prestador': id_prestador})
 
+def TransporteListView(request, id_prestador):
+    # Cria um objeto vazio chamado transporte_list
+    transporte_list = None
+
+    #Aplica o filtro sobre os objetos em Transporte
+    v_transporte_filtrado = Transporte.objects.filter(prestador=id_prestador)
+
+    # Verifica se foi encontrado algum registro
+    if len(v_transporte_filtrado) > 0 :
+        transporte_list = v_transporte_filtrado
+    else:
+        transporte_list = None
+
+    # Retorna o template transporte_lista.html e passa o filtro como parâmetro
+    return render(request, 'app_principal/transporte_list.html', {'transporte_list': transporte_list})
+
+def TransporteContratoListView(request, id_veiculo):
+    # Cria um objeto vazio chamado transporte_list
+    transporte_contrato_list = None
+
+    #Aplica o filtro sobre os objetos em Transporte
+    v_transporte_contrato_filtrado = Contrato.objects.filter(transporte_id=id_veiculo)
+
+    # Verifica se foi encontrado algum registro
+    if len(v_transporte_contrato_filtrado) > 0 :
+        transporte_contrato_list = v_transporte_contrato_filtrado
+    else:
+        transporte_contrato_list = None
+
+    # Retorna o template transporte_lista.html e passa o filtro como parâmetro
+    return render(request, 'app_principal/transporte_contrato_list.html', {'transporte_contrato_list': transporte_contrato_list})
+
+def TransporteListTodosView(request, id_cliente):
+    # Cria um objeto vazio chamado transporte_list
+    transporte_list = None
+
+    #Aplica o filtro sobre os objetos em Transporte
+    v_transporte_filtrado = Transporte.objects.all
+
+    # Verifica se foi encontrado algum registro
+    # if len(v_transporte_filtrado) > 0 :
+    transporte_list = v_transporte_filtrado
+    # else:
+    # transporte_list = None
+
+    # Retorna o template transporte_lista.html e passa o filtro como parâmetro
+    return render(request, 'app_principal/transporte_list_todos.html', {'transporte_list': transporte_list, 'id_cliente': id_cliente})
